1084b6c3da5ed759b78d55e0dc044587938aef4b
sven-k <sven-k@738a3cfe-f54d-0410-852a-ebac1faf349d>
2008-11-12 22:04:39 +0000

    builder stuff

--- org.svenk.redmine-builder/build.sh
+++ org.svenk.redmine-builder/build.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
+DIR=`echo $0 | sed -e 's/^\(.\+\/\).*$/\1/'`
 
-svn co https://redmin-mylyncon.svn.sourceforge.net/svnroot/redmin-mylyncon/trunk ./svnfetch
+if [ ! -d ./svnfetch ];then
+	svn co https://redmin-mylyncon.svn.sourceforge.net/svnroot/redmin-mylyncon/trunk ${DIR}svnfetch
+fi
 
-java -jar /home/sven/bin/eclipseSDK/plugins/org.eclipse.equinox.launcher_1.0.101.R34x_v20080819.jar -application org.eclipse.ant.core.antRunner -buildfile /home/sven/bin/eclipseSDK/plugins/org.eclipse.pde.build_3.4.1.R34x_v20080805/scripts/build.xml -Dbuilder=/home/sven/Workspaces/rcp/org.svenk.redmine-builder/builder
+ant -propertyfile buildserver.properties
+
