a300bac170a053ed39bd8f42d16a573b102144b2
sven-k <sven-k@738a3cfe-f54d-0410-852a-ebac1faf349d>
2008-11-14 11:55:21 +0000

    build script

--- org.svenk.redmine-builder/build.sh
+++ org.svenk.redmine-builder/build.sh
@@ -2,6 +2,10 @@
 
 DIR=`echo $0 | sed -e 's/^\(.\+\/\).*$/\1/'`
 
+# Update of build script
+svn update ${DIR}
+
+# initial checkout of sources
 if [ ! -d ./svnfetch ];then
 	svn co https://redmin-mylyncon.svn.sourceforge.net/svnroot/redmin-mylyncon/trunk ${DIR}svnfetch
 fi
