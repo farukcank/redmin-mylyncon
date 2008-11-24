fe1c9076c27a61ee91bdb2208457fe8029006d5d
sven-k <sven-k@738a3cfe-f54d-0410-852a-ebac1faf349d>
2008-11-24 18:52:14 +0000

    Add EPL header

--- org.svenk.redmine.core/aspectj/org/svenk/redmine/core/qualitycontrol/RedmineClientExceptionAspect.java
+++ org.svenk.redmine.core/aspectj/org/svenk/redmine/core/qualitycontrol/RedmineClientExceptionAspect.java
@@ -1,3 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Mylyn project committers and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Mylyn project committers
+ *******************************************************************************/
+/*******************************************************************************
+ * Copyright (c) 2008 Sven Krzyzak
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Sven Krzyzak - adapted Trac implementation for Redmine
+ *******************************************************************************/
 package org.svenk.redmine.core.qualitycontrol;
 
 import org.svenk.redmine.core.RedmineCorePlugin;
--- org.svenk.redmine.core/src/org/svenk/redmine/core/util/internal/XmlRpcCommonsTransport.java
+++ org.svenk.redmine.core/src/org/svenk/redmine/core/util/internal/XmlRpcCommonsTransport.java
@@ -1,47 +1,22 @@
 /*******************************************************************************
- *
- * Redmine-Mylyn-Connector
- * 
- * This implementation is on the basis of the implementations of Trac and 
- * Bugzilla emerged and contains parts of source code from these projects.
- * The corresponding copyright notice follows below of this.
- * Copyright (C) 2008  Sven Krzyzak and others
- *  
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the 
- * Free Software Foundation; either version 3 of the License, 
- * or (at your option) any later version.
- *  
- * This program is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
- * more details.
- *  
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, see <http://www.gnu.org/licenses/>.
- *  
- *******************************************************************************/
-/*******************************************************************************
  * Copyright (c) 2004, 2007 Mylyn project committers and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Mylyn project committers
  *******************************************************************************/
 /*******************************************************************************
- * Copyright 1999,2005 The Apache Software Foundation.
+ * Copyright (c) 2008 Sven Krzyzak
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Contributors:
+ *     Sven Krzyzak - adapted Trac implementation for Redmine
  *******************************************************************************/
 package org.svenk.redmine.core.util.internal;
 
