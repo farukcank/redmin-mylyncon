0091bebdce8229065995958a2814d8c6c5e607d5
sven-k <sven-k@738a3cfe-f54d-0410-852a-ebac1faf349d>
2009-09-06 11:07:39 +0000

    add EOL tag

--- eclipse_mylyn_connector/init.rb
+++ eclipse_mylyn_connector/init.rb
@@ -6,6 +6,6 @@ RAILS_DEFAULT_LOGGER.info 'Starting mylyn-connector plugin for RedMine'
 Redmine::Plugin.register :mylyn_connector do
   name 'Mylyn connector plugin'
   author 'Sven Krzyzak'
-  description 'This plugin provides a webservice API for Eclipse Mylyn'
+  description 'This plugin provides a webservice API for Eclipse Mylyn (XML-RPC - requires ActionWebService -- EOL)'
   version 'v2.5'
 end
